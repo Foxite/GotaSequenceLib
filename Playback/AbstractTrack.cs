b38459a0e582d4adb4518550763e76f23793ff3b
Dirk Kok <the@dirkkok.nl>
2022-06-13 14:36:26 +0200

    convert to net6.0

--- Playback/AbstractTrack.cs
+++ Playback/AbstractTrack.cs
@@ -19,33 +19,33 @@ namespace GotaSequenceLib.Playback {
         public bool NoteDown;
 
         // *SEQ properties 
-        abstract public bool Tie { set; }
-        abstract public bool NoteWait { set; } // previously known as Mono
-        abstract public bool Portamento { set; }
-        abstract public int Voice { set; }
-        abstract public byte Priority { set; }
-        abstract public byte Volume { set; }
-        abstract public byte Expression { set; }
-        abstract public byte LFORange { set; }
-        abstract public byte PitchBendRange { set; }
-        abstract public byte LFOSpeed { set; }
-        abstract public byte LFODepth { set; }
-        abstract public ushort LFODelay { set; }
-        abstract public ushort LFOPhase { set; }
-        abstract public ushort LFODelayCount { set; }
-        abstract public LFOType LFOType { set; }
-        abstract public sbyte PitchBend { set; }
-        abstract public sbyte Panpot { set; }
-        abstract public sbyte Transpose { set; }
-        abstract public byte Attack { set; }
-        abstract public byte Decay { set; }
-        abstract public byte Sustain { set; }
-        abstract public byte Hold { set; }
-        abstract public byte Release { set; }
-        abstract public byte PortamentoKey { set; }
-        abstract public byte PortamentoTime { set; }
-        abstract public short SweepPitch { set; }
-        abstract public int BankNum { set; }
+        abstract public bool Tie { get; set; }
+        abstract public bool NoteWait { get; set; } // previously known as Mono
+        abstract public bool Portamento { get; set; }
+        abstract public int Voice { get; set; }
+        abstract public byte Priority { get; set; }
+        abstract public byte Volume { get; set; }
+        abstract public byte Expression { get; set; }
+        abstract public byte LFORange { get; set; }
+        abstract public byte PitchBendRange { get; set; }
+        abstract public byte LFOSpeed { get; set; }
+        abstract public byte LFODepth { get; set; }
+        abstract public ushort LFODelay { get; set; }
+        abstract public ushort LFOPhase { get; set; }
+        abstract public ushort LFODelayCount { get; set; }
+        abstract public LFOType LFOType { get; set; }
+        abstract public sbyte PitchBend { get; set; }
+        abstract public sbyte Panpot { get; set; }
+        abstract public sbyte Transpose { get; set; }
+        abstract public byte Attack { get; set; }
+        abstract public byte Decay { get; set; }
+        abstract public byte Sustain { get; set; }
+        abstract public byte Hold { get; set; }
+        abstract public byte Release { get; set; }
+        abstract public byte PortamentoKey { get; set; }
+        abstract public byte PortamentoTime { get; set; }
+        abstract public short SweepPitch { get; set; }
+        abstract public int BankNum { get; set; }
 
         public class TrackVars {
             private readonly Player _player;
